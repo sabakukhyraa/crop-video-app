aa8ab4d74e85fcdf30a030ebcc9a646b29c1ff56
sabakukhyraa <alikerematas@gmail.com>
2025-02-23 17:03:01 +0300

    Fill slug files with temporary components to prevent errors

---
+++ app/edit/[videoId].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const EditVideo = () => {
+  return (
+    <View>
+      <Text>EditVideo</Text>
+    </View>
+  );
+};
+
+export default EditVideo;

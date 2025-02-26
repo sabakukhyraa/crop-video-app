6cc71798b26543c531056bcb8125b1a3d5ef2a5b
sabakukhyraa <alikerematas@gmail.com>
2025-02-27 01:36:31 +0300

    Build metadata modal

--- app/video-modals/metadata.tsx
+++ app/video-modals/metadata.tsx
@@ -1,17 +1,120 @@
-import { View, Text } from "react-native";
-import React from "react";
 import BaseButton from "@components/BaseButton";
+import ModalHeader from "@components/ModalHeader";
 import ThemedText from "@components/ThemedText";
-import { router } from "expo-router";
+import Colors from "@constants/Colors";
+import tw from "@lib/tailwind";
+import { router, useNavigation } from "expo-router";
+import { ActivityIndicator, Platform, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as FileSystem from "expo-file-system";
+import { useEffect, useRef } from "react";
+import { useBoundStore } from "@store/useBoundStore";
+import MetadataForm, { MetadataFormHandles } from "@components/MetadataForm";
+import { useCropVideoMutation } from "mutations/useCropVideoMutation";
 
-const Metadata = () => {
+const Crop = () => {
+  const navigation = useNavigation();
+
+  const {
+    mutate,
+    isPending,
+    isError,
+    error,
+    data: mutationData,
+  } = useCropVideoMutation();
+
+  const selectedVideo = useBoundStore((state) => state.selectedVideo);
+  const cleanSelectedVideo = useBoundStore(
+    (state: { cleanSelectedVideo: () => void }) => state.cleanSelectedVideo
+  );
+
+  const metadataFormRef = useRef<MetadataFormHandles | null>(null);
+
+  const handleCropAndCreate = () => {
+    const errors = metadataFormRef.current?.submit();
+    console.log("Form Errors:", errors);
+
+    if (errors) {
+      if (errors.name || errors.description) {
+        return;
+      }
+    }
+
+    if (selectedVideo?.cropStartTime) {
+      mutate({
+        uri: selectedVideo.uri,
+        start: selectedVideo.cropStartTime,
+        id: Date.now().toString(),
+      });
+    }
+
+    console.log(mutationData);
+  };
+
+  // -----------------------------------------------------
+  // CLEANUP
+  // -----------------------------------------------------
+  // Cleanup the selected video from cache
+  useEffect(() => {
+    return () => {
+      (async () => {
+        try {
+          await FileSystem.deleteAsync(selectedVideo!.uri, {
+            idempotent: true,
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      })();
+    };
+  }, []);
+
+  // Clean the store on modal exit
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("beforeRemove", () => {
+      cleanSelectedVideo();
+    });
+    return unsubscribe;
+  }, [navigation, cleanSelectedVideo]);
+
   return (
-    <View>
-      <BaseButton onPress={() => router.dismissAll()}>
-        <ThemedText>Bass</ThemedText>
-      </BaseButton>
-    </View>
+    <SafeAreaView
+      style={tw.style(
+        "flex-1",
+        Platform.OS === "ios" ? "bg-darkGray" : "bg-midGray"
+      )}
+    >
+      {/* Header */}
+      <ModalHeader content="Add Metadata" onClose={() => router.back()} />
+
+      {/* Seperator */}
+      <View style={tw.style("w-full h-px bg-lightGray opacity-10")} />
+
+      <View style={tw.style("container bg-darkGray")}>
+        {isPending ? (
+          <ActivityIndicator size="large" color={Colors.lightGray} />
+        ) : (
+          <MetadataForm ref={metadataFormRef} />
+        )}
+
+        <View style={tw`flex-1`} />
+
+        <BaseButton
+          onPress={handleCropAndCreate}
+          style={tw.style("button-icon")}
+        >
+          <ThemedText
+            color={Colors.darkGray}
+            lineHeight={18}
+            weight={600}
+            size={16}
+          >
+            Crop and Create the Video
+          </ThemedText>
+        </BaseButton>
+      </View>
+    </SafeAreaView>
   );
 };
 
-export default Metadata;
+export default Crop;

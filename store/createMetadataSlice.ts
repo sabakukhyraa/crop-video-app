dbe444a3ee46c7a28cda3dbd72d60cf96130aafd
sabakukhyraa <alikerematas@gmail.com>
2025-02-27 01:35:11 +0300

    Add cleanBoth action to Metadata slice to clear the states to their initial state

--- store/createMetadataSlice.ts
+++ store/createMetadataSlice.ts
@@ -1,20 +1,24 @@
-import { StateCreator } from 'zustand';
+import { StateCreator } from "zustand";
 
 export interface MetadataSlice {
   videoName: string;
   videoDescription: string;
   setVideoName: (name: string) => void;
   setVideoDescription: (desc: string) => void;
+  cleanBoth: () => void;
 }
 
 export const createMetadataSlice: StateCreator<MetadataSlice> = (set) => ({
-  videoName: '',
-  videoDescription: '',
+  videoName: "",
+  videoDescription: "",
 
   setVideoName: (name) => {
     set({ videoName: name });
   },
   setVideoDescription: (desc) => {
     set({ videoDescription: desc });
+  },
+  cleanBoth: () => {
+    set({ videoName: "", videoDescription: "" });
   },
 });
--- store/useBoundStore.ts
+++ store/useBoundStore.ts
@@ -1,10 +1,13 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { create } from 'zustand';
-import { devtools, persist, createJSONStorage } from 'zustand/middleware';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { create } from "zustand";
+import { devtools, persist, createJSONStorage } from "zustand/middleware";
 
-import { createVideoSlice, VideoSlice } from './createVideoSlice';
-import { createMetadataSlice, MetadataSlice } from './createMetadataSlice';
-import { createSelectedVideoSlice, SelectedVideoSlice } from './createSelectedVideoSlice';
+import { createVideoSlice, VideoSlice } from "./createVideoSlice";
+import { createMetadataSlice, MetadataSlice } from "./createMetadataSlice";
+import {
+  createSelectedVideoSlice,
+  SelectedVideoSlice,
+} from "./createSelectedVideoSlice";
 
 export type AppState = VideoSlice & SelectedVideoSlice & MetadataSlice;
 
@@ -17,7 +20,7 @@ export const useBoundStore = create<AppState>()(
         ...createSelectedVideoSlice(...args),
       }),
       {
-        name: 'video-storage',
+        name: "video-storage",
         storage: createJSONStorage(() => AsyncStorage),
         partialize: (state) => {
           const {

ca2f54362ed770b6832b7e0bdeaec0d2a43944ed
sabakukhyraa <alikerematas@gmail.com>
2025-02-27 14:10:30 +0300

    Update button-icon style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
-const { default: Colors } = require('constants/Colors');
-const plugin = require('tailwindcss/plugin');
+const { default: Colors } = require("constants/Colors");
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-	theme: {
-		fontFamily: {
-			ralewaybold: ['Raleway-Bold', 'sans-serif'],
-			ralewaysemibold: ['Raleway-SemiBold', 'sans-serif'],
-			ralewaymedium: ['Raleway-Medium', 'sans-serif'],
-			raleway: ['Raleway-Regular', 'sans-serif'],
-			ralewaylight: ['Raleway-Light', 'sans-serif'],
-		},
-		colors: {
-			darkGray: Colors.darkGray,
-			midGray: Colors.midGray,
-			teal: Colors.teal,
-			lightGray: Colors.lightGray
-		},
-	},
-	plugins: [
-		plugin(({ addUtilities }) => {
-			addUtilities({
-				'container': "flex-1 p-5 gap-4 items-center",
-				'button-icon': "flex-row items-center gap-2"
-			});
-		}),
-	],
+  theme: {
+    fontFamily: {
+      ralewaybold: ["Raleway-Bold", "sans-serif"],
+      ralewaysemibold: ["Raleway-SemiBold", "sans-serif"],
+      ralewaymedium: ["Raleway-Medium", "sans-serif"],
+      raleway: ["Raleway-Regular", "sans-serif"],
+      ralewaylight: ["Raleway-Light", "sans-serif"],
+    },
+    colors: {
+      darkGray: Colors.darkGray,
+      midGray: Colors.midGray,
+      teal: Colors.teal,
+      lightGray: Colors.lightGray,
+    },
+  },
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        container: "flex-1 p-5 gap-4 items-center",
+        "button-icon": "flex-row items-center gap-1",
+      });
+    }),
+  ],
 };
